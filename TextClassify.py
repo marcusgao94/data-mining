c26d19468bc52cf266eabf07001b1f74c881232b
gy <marcusgao@163.com>
2015-06-29 19:34:03 +0800

    train and cross-validate using svm, naive bayes, logistic regression, score using accuracy, precision, recall

--- TextClassify.py
+++ TextClassify.py
@@ -1,15 +1,16 @@
-import os
-import numpy
+import os, numpy, time
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
-from sklearn.datasets import make_multilabel_classification
+from sklearn.cross_validation import cross_val_score
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model.logistic import LogisticRegression
 
-content = []
+X = []
 label = []
-test_content = []
+test_X = []
 
 def readFile(filePath):
     con = []
@@ -31,43 +32,39 @@ def readFile(filePath):
     return (con, lab)
 
 BASEPATH = '../data'
-years = ['1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996']
-cnt = 0
+years = ['1987']#, '1988', '1989', '1990', '1991']
+print 'reading in'
 for year in years:
     if year.startswith('.'):
         continue
     print year
     yearPath = BASEPATH + '/' + year
     (con, lab) = readFile(yearPath + '/body.txt')
-    content.extend(con)
+    X.extend(con)
     label.extend(lab)
-    cnt += 1
-    if cnt == 1:
-        break
 
-(test_content, lab) = readFile(BASEPATH + '/1996/body.txt')
+print '%d documents' % len(X)
 
-l = len(content)
-print l
-vec = TfidfVectorizer()
 bin = MultiLabelBinarizer()
-a = vec.fit_transform(content)
-b = bin.fit_transform(label)
-analyze = vec.build_analyzer()
-classifier = Pipeline([('vectorizer', TfidfVectorizer()),
-                       ('clf', OneVsRestClassifier(LinearSVC()))])
-classifier.fit(content, b)
-predicted = classifier.predict(test_content)
-print bin.inverse_transform(predicted)
-
+y = bin.fit_transform(label)
+classifiers = [LinearSVC(), BernoulliNB(), LogisticRegression()]
+clfNames = ['svm', 'naive bayes', 'logistic regression']
+scorings = ['accuracy', 'precision', 'recall']
+for i in range(0, 3):
+    classifier = Pipeline([('vectorizer', TfidfVectorizer()),
+                            ('clf', OneVsRestClassifier(classifiers[i]))])
+    print 'training ' + clfNames[i]
+    for scoring in scorings:
+        t1 = time.clock()
+        scores = cross_val_score(classifier, X, y, cv = 5, scoring=scoring)
+        t2 = time.clock()
+        #print 'trained for %ds' % (t2 - t1)
+        print '%s =' % scoring,
+        for cvs in scores:
+            print '%.1f%%' % (cvs * 100),
+        print ''
 """
-numpy.set_printoptions(threshold='nan')
-
-
-classifier = OneVsRestClassifier(LinearSVC())
-print 'begin training'
-classifier.fit(a[0:-5], b[0:-5])
-print 'begin predicting'
-pre = classifier.predict(a[-1])
-print bin.inverse_transform(pre)
+    classifier.fit(X, y)
+    predicted = classifier.predict(test_X)
+    #li = bin.inverse_transform(predicted)
 """
